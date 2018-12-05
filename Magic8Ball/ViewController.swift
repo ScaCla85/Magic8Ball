7f97eecdcc0f951d8796b7c52ddd895d4059b420
Claudio Scarpellini <claudio.scarpellini@ozdigital.com>
2018-12-05 14:28:33 +0100

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Claudio Scarpellini on 05/12/18.
+//  Copyright © 2018 Claudio Scarpellini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
